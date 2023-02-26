cba2a41e10af3553ee613e6acc35ad6212ad9632
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2023-02-26 04:41:03 -0300

    Implements Custom Routing per Profile

--- setup.sh
+++ setup.sh
@@ -12,35 +12,47 @@ function set_working_dir() {
     envsubst < $WORK_DIR/backend/backend-secret.template.yaml > $WORK_DIR/backend/backend-secret.yaml;
     envsubst < $WORK_DIR/postgres/postgres-secret.template.yaml > $WORK_DIR/postgres/postgres-secret.yaml;
     envsubst < $WORK_DIR/redis/redis-secret.template.yaml > $WORK_DIR/redis/redis-secret.yaml;
+    
+    envsubst < $WORK_DIR/nginx-ingress/nginx-ingress-root-template.yaml >  $WORK_DIR/nginx-ingress/nginx-ingress-root.yaml
+    envsubst < $WORK_DIR/nginx-ingress/nginx-ingress-api-template.yaml >  $WORK_DIR/nginx-ingress/nginx-ingress-api.yaml
+    
     rm $WORK_DIR/frontend/frontend-secret.template.yaml; 
     rm $WORK_DIR/redis/redis-secret.template.yaml; 
     rm $WORK_DIR/postgres/postgres-secret.template.yaml; 
     rm $WORK_DIR/backend/backend-secret.template.yaml;
 
+    rm $WORK_DIR/nginx-ingress/nginx-ingress-root-template.yaml
+    rm $WORK_DIR/nginx-ingress/nginx-ingress-api-template.yaml
+
 }
 
 function main {
-
-    if [[ $1 == "--test" || $1 == "-t" ]]; then
         
-        if [[ -e .secret ]]; then
+    if [[ -e .secret ]]; then
 
-            set -a
-            while read line; do
+        set -a
+
+        if [[ $1 == "--staging" || $1 == "-a" ]]; then
+            DOMAIN="staging.hideyoshi.com.br"
+        else 
+            DOMAIN="hideyoshi.com.br"
+        fi
+
+        while read line; do
+            if [[ $line != "" ]]; then
                 variable=$(echo -n "$line" | cut -f 1 -d '=')
                 value=$(echo -n $(echo -n "$line" | cut -f 2 -d '=') | base64)
                 declare $variable=$value
-            done < ./.secret
-            
-            set +a
+            fi
+        done < ./.secret
 
-        else 
+        set +a
 
-            echo "ERROR: Secret file not found.";
-            exit 1;
+    else 
 
-        fi
-    
+        echo "ERROR: Secret file not found.";
+        exit 1;
+
     fi
 
     set_working_dir
