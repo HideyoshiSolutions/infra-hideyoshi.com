0061def7593ca27b49d44ab4cb31c10175bdd158
Vitor Hideyoshi <vitor.h.n.batista@gmail.com>
2023-05-28 03:13:11 -0300

    Rewriting Setup Tools - Adds CertManager

--- setup.sh
+++ setup.sh
@@ -16,23 +16,27 @@ function set_working_dir() {
     envsubst < $TEMPLATE/postgres/postgres-secret.template.yaml > $WORK_DIR/postgres/postgres-secret.yaml;
     envsubst < $TEMPLATE/frontend/frontend-secret.template.yaml > $WORK_DIR/frontend/frontend-secret.yaml;
     envsubst < $TEMPLATE/backend/backend-secret.template.yaml > $WORK_DIR/backend/backend-secret.yaml;
+    envsubst < $TEMPLATE/cert-manager/cert-manager-certificate.template.yaml > $WORK_DIR/cert-manager/cert-manager-certificate.yaml;
 
 }
 
 function main {
 
-    [[ -z $1 ]] && file=".secret" || file=$1
+    [[ -z $1 ]] && secret_file=".secret" || file=$1
 
-    if [[ -e $file ]]; then
+    if [[ -e $secret_file ]]; then
 
         set -a
         
         if [[ $1 == "--dev" || $1 == "-d" ]]; then
             DOMAIN="dev.hideyoshi.com.br"
+            API_DOMAIN="api.dev.hideyoshi.com.br"
         elif [[ $1 == "--staging" || $1 == "-a" ]]; then
             DOMAIN="staging.hideyoshi.com.br"
+            API_DOMAIN="api.staging.hideyoshi.com.br"
         else 
             DOMAIN="hideyoshi.com.br"
+            API_DOMAIN="api.hideyoshi.com.br"
         fi
 
         while read line; do
@@ -41,7 +45,7 @@ function main {
                 value=$(echo -n $(echo -n "$line" | cut -f 2 -d '=') | base64)
                 declare $variable=$value
             fi
-        done < $file
+        done < $secret_file
 
         set +a
 
